926d929e9b7275f833a42d59bbf46438cdad510d
Graham Sabin <grahamcsabin@outlook.com>
2022-01-17 10:16:18 -0800

    initial commit with authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Dashboard from './Dashboard/Dashboard';
+import Login from './Components/Login/Login';
+import Preferences from './Preferences/Preferences';
+import useToken from './Components/App/useToken';
+
+
 
 function App() {
+/*   const [token, setToken] = useState(); */
+
+  const {token, setToken} = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
